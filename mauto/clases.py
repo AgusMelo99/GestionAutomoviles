cb0f4ea7f9c932980ba98e3a8b972863da58853e
Julian Cortes <juliancortes729@gmail.com>
2024-08-31 17:45:38 -0300

    Primer commit
    archivos iniciales

---
+++ mauto/clases.py
@@ -0,0 +1,14 @@
+
+class Usuario:
+    def __init__(self, nombre, email, contra):
+        self.nombre = nombre
+        self.email = email
+        self.contra = contra
+
+#aca hace falta enviar los datos instanciados a bd
+    def registrarse():
+        pass
+
+#esto deberia comparar los datos enviados del cliente 
+    def login():
+        pass

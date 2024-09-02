ae006cd3a3433beaaaaccb05940705daa5aa9077
LeoFernandez <nizares97@gmail.com>
2024-09-02 05:41:26 -0300

    refactor  main

--- main.py
+++ main.py
@@ -1,46 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 
-from database import crear_usuario, init_app
-
 from werkzeug.security import generate_password_hash
 
 from config import Config
 
-app = Flask(__name__, template_folder='front/html', static_folder='front/static')
-app.config.from_object(Config)
- 
-# Inicialización de la base de datos MySQL
-init_app(app)
+from database import *
 
+#instancia de la app
+app = Flask(__name__, template_folder='front/html', static_folder='front/static')
+ 
+# instancia de la base de datos
+
+db = ConexionBD(Config.MYSQL_HOST,
+                Config.MYSQL_PORT,
+                Config.MYSQL_USER,
+                Config.MYSQL_PASSWORD,
+                Config.MYSQL_DB)
+
+#ruta de pagina principal
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#ruta para iniciar sesion
 @app.route('/inicio', methods=['GET', 'POST'])
 def inicio():
-    if request.method == 'POST':
-        email = request.form['email']
-        contrasena = request.form['contrasena']
-        #lógica de autenticación
-        #se guardan las variables mail y contrasena asi las usamos en la DB
-        return redirect(url_for('index'))
-    return render_template('inicio.html')
+    if request.method == 'GET':
+        return render_template('inicio.html')
+    email = str(request.form['email'])
+    contrasena = str(request.form['contrasena'])
 
+    #falta la logica para autenticar
+
+#ruta para registrar cuenta
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        apellido = request.form['apellido']
-        email = request.form['email']
-        contrasena = generate_password_hash(request.form['contrasena'])
-        #lógica de registro
-        #lo mismo que en def inicio pero con registro
+    if request.method == 'GET':
+        return render_template('registro.html')
+    
+    #estandarizamos los datos para usarlos
+    nombre = str(request.form['nombre'])
+    apellido = str(request.form['apellido'])
+    email = str(request.form['email'])
+    contrasena = str(generate_password_hash(request.form['contrasena']))
 
-        crear_usuario(nombre, apellido, email, contrasena)
-        flash('Usuario registrado exitosamente', 'success')
+    #logica de registro
+    db.crear_usuario(nombre, apellido, email, contrasena)
 
-        return redirect(url_for('index'))
-    return render_template('registro.html')
+    flash('Usuario registrado exitosamente', 'success')
+    return redirect(url_for('index'))
 
+#ejecucion de la app
 if __name__ == '__main__':
     app.run(debug=True)
